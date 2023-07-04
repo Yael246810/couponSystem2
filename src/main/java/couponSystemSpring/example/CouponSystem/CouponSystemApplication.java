2b14bb2b0207a0d6822d88f34d4eb1a13b47d0c3
Yael246810 <yaelcohen691@gmail.com>
2023-07-04 22:09:15 +0300

    updated project 24/6

--- src/main/java/couponSystemSpring/example/CouponSystem/CouponSystemApplication.java
+++ src/main/java/couponSystemSpring/example/CouponSystem/CouponSystemApplication.java
@@ -11,8 +11,5 @@ public class CouponSystemApplication {
 		System.out.println("Start");
 		ApplicationContext ctx = SpringApplication.run(CouponSystemApplication.class, args);
 		System.out.println("End");
-
-		//TODO: problems in Admin - get all companies, get all customers (in Postman) and get customer coupons by price gives an empty list
-		//TODO: the customers cannot do Login.... and the methods suddenly don't work - the problem is with the method: LoginManager. and to make sure that I use: IsUserAllowed.
 	}
 }
